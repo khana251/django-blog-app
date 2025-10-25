297a93e62721459b4041b6dd984399bb8bbba61d
amaan-k <59571647+amaan-k@users.noreply.github.com>
2025-10-25 14:00:59 -0400

    Initial commit -Django Blog App

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+posts = [
+    {
+        'author': 'Amaan',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 22, 2025'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 23, 2025'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
