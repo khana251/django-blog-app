301b3b7464edba9d32ce78afcf7b3b8e4bf4368d
amaan-k <59571647+amaan-k@users.noreply.github.com>
2025-10-26 20:24:13 -0400

    Testing members app

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'members.apps.MembersConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
